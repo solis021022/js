bfed03324ee04f32e818bf4e5aa154cea00deb6e
Abraham Solis <wakkosol@gmail.com>
2024-04-17 12:58:06 -0500

    first commit

---
+++ menu/app.js
@@ -0,0 +1,17 @@
+let im=document.querySelector("#im");
+let btn1=document.querySelector("#btn1");
+let btn2=document.querySelector("#btn2");
+let btn3=document.querySelector("#btn3");
+let btn4=document.querySelector("#btn4");
+document.querySelector("#btn1").onclick=()=>{
+    im.innerHTML="<img src='barca.jpg' width=100px>";
+  }
+  document.querySelector("#btn2").onclick=()=>{
+    im.innerHTML="<img src='uno.jpg' width=100px>";
+  }
+  document.querySelector("#btn3").onclick=()=>{
+    im.innerHTML="<img src='golden.jpg' width=100px>";
+  }
+  document.querySelector("#btn4").onclick=()=>{
+    im.innerHTML="<img src='ney.jpg' width=100px>";
+  }
